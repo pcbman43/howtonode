e4d9df9218e628eaa467df25db76992eff24f54f
MKougia <kougia.mihkel@khk.ee>
2022-02-02 15:11:31 +0200

    Node.js File System; Example 5 - Updating files using append

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-fs.writeFile('text_file3.txt', 'Hello content!', function (err) {
+fs.appendFile('text_file.txt', ' This is my text.', function (err) {
   if (err) throw err;
-  console.log('Saved!');
-}); 
+  console.log('Updated!');
+});
